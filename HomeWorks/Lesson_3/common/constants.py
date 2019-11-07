5c5cab273d9c00e545f2ca98997fea2af09c18af
OlegSP <spoliv@rambler.ru>
2019-11-08 01:37:35 +0300

    Lesson_3

---
+++ HomeWorks/Lesson_3/common/constants.py
@@ -0,0 +1,14 @@
+DEFAULT_PORT = 9999
+DEFAULT_ADDRESS = '127.0.0.1'
+MAX_CONNECTED = 8
+MAX_PACKAGE_SIZE = 1024
+ENCODING = 'utf-8'
+
+# Ключи протокола JIM
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
